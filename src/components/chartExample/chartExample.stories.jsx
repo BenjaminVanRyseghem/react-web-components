663bacac4614071495d753939b1f453456b91c0c
Damien Cassou <damien@cassou.me>
2021-06-21 22:16:08 +0200

    WIP

---
+++ src/components/chartExample/chartExample.stories.jsx
@@ -0,0 +1,9 @@
+import ChartExample from "./chartExample";
+import React from "react";
+
+export default {
+	component: ChartExample,
+	title: "Components/ChartExample"
+};
+
+export const shows = () => <ChartExample/>;
