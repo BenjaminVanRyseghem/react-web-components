6adf3cc8f9a046aa2364157f538c2d0878e27f3b
Damien Cassou <damien@cassou.me>
2021-06-24 09:59:15 +0200

    Add profitAndLoss widget

---
+++ src/widgets/profitAndLoss/profitAndLoss.jsx
@@ -0,0 +1,89 @@
+import annualTrend from "./annual-trend.json";
+import FakeLoadingComponent from "components/fakeLoadingComponent/fakeLoadingComponent";
+import LoadingChart from "components/loadingChart/loadingChart";
+import ProfitAndLossChart from "components/profitAndLossChart/profitAndLossChart";
+import React from "react";
+import registerComponent from "helpers/registerComponent";
+import withWKStyle from "hoc/withWKStyle";
+
+const dates = [
+	new Date(2020, 0, 1),
+	new Date(2020, 1, 1),
+	new Date(2020, 2, 1),
+	new Date(2020, 3, 1),
+	new Date(2020, 4, 1),
+	new Date(2020, 5, 1),
+	new Date(2020, 6, 1),
+	new Date(2020, 7, 1),
+	new Date(2020, 8, 1),
+	new Date(2020, 9, 1)
+];
+
+const data = {
+	labels: dates,
+	datasets: [
+		{
+			label: "Loss",
+			data: annualTrend.profitTrend,
+			borderColor: "#F00",
+			hoverBackgroundColor: "#FED009",
+			backgroundColor: "#EA8F00",
+			order: 2,
+			yAxisID: "currency-axis",
+			legendOrder: 0
+		},
+		{
+			label: "YTD Profit",
+			data: annualTrend.accumulatedProfitTrend,
+			borderColor: "#85BC20",
+			backgroundColor: "#eee",
+			type: "line",
+			order: 0,
+			yAxisID: "currency-axis",
+			legendOrder: 1,
+			pointRadius: 7,
+			pointHoverRadius: 10,
+			pointBorderWidth: 3,
+			pointHoverBorderWidth: 3
+		},
+		{
+			label: "Income",
+			data: annualTrend.incomeTrend,
+			borderColor: "#F00",
+			hoverBackgroundColor: "#888888",
+			backgroundColor: "#474747",
+			order: 1,
+			yAxisID: "currency-axis",
+			legendOrder: 2,
+			hidden: true
+		},
+		{
+			label: "YTD Income",
+			data: annualTrend.accumulatedIncomeTrend,
+			borderColor: "#474747",
+			backgroundColor: "#eee",
+			type: "line",
+			order: 3,
+			yAxisID: "currency-axis",
+			legendOrder: 3,
+			hidden: true,
+			pointRadius: 7,
+			pointHoverRadius: 10,
+			pointBorderWidth: 3,
+			pointHoverBorderWidth: 3
+
+		}
+	]
+};
+
+function ProfitAndLoss() {
+	return (
+		<FakeLoadingComponent as="chartData" data={data} loader={<LoadingChart/>}>
+			<ProfitAndLossChart/>
+		</FakeLoadingComponent>
+	);
+}
+
+ProfitAndLoss.nodeName = "finsit-profit-and-loss";
+
+export default registerComponent(withWKStyle(ProfitAndLoss));
---
+++ src/widgets/profitAndLoss/profitAndLoss.stories.jsx
@@ -0,0 +1,9 @@
+import ProfitAndLoss from "./profitAndLoss";
+import React from "react";
+
+export default {
+	component: ProfitAndLoss,
+	title: "Widgets/ProfitAndLoss"
+};
+
+export const shows = () => <ProfitAndLoss/>;
