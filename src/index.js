6c40410b12569063b011cc33e4901e5a25c535c3
Nicolas Petton <nicolas@petton.fr>
2021-06-24 15:44:57 +0200

    Add profitAndLossExpanded to index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "./widgets/profitAndLoss/profitAndLoss";
+import "./widgets/profitAndLossExpanded/profitAndLossExpanded";
 import "./widgets/company/company";
 import "./widgets/periodReportArchive/periodReportArchive";
