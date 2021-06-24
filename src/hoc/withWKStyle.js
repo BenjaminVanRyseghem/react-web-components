1eed353637f5525fac38d7bd950542ffddfa5a36
Nicolas Petton <nicolas@petton.fr>
2021-06-24 10:01:33 +0200

    Fix withWKStyle WK CSS loading

--- src/hoc/withWKStyle.js
+++ src/hoc/withWKStyle.js
@@ -5,13 +5,22 @@ import React from "react";
  * Inject WK style inside the shadow DOM
  */
 
-const wkCssFundamentalsPath = "https://files.petton.fr/cassou/temp/all.min.css";
+const wkCssFundamentalsPath = "https://cdn.wolterskluwer.io/wk/fundamentals/1.x.x/all.min.css";
+const wkCssComponentsPath = "https://cdn.wolterskluwer.io/wk/components/1.x.x/all.min.css";
+const wkCssTypographyPath = "https://cdn.wolterskluwer.io/wk/fundamentals/1.x.x/typography.min.css";
+const wkCssFiraSansPath = "https://cdn.wolterskluwer.io/wk/fundamentals/1.x.x/typography/fira-sans/all.min.css";
+const wkCssFiraRegularPath = "https://cdn.wolterskluwer.io/wk/fundamentals/1.x.x/typography/fira-sans/regular.min.css";
 
 export default function withWKStyle(Component) {
 	let fn = () => (
 		<>
-			<link href={wkCssFundamentalsPath} rel="stylesheet"/>
-			<Component/>
+		  <link href={wkCssFundamentalsPath} rel="stylesheet"/>
+		  <link href={wkCssFundamentalsPath} rel="stylesheet" />
+		  <link href={wkCssComponentsPath} rel="stylesheet" />
+		  <link href={wkCssTypographyPath} rel="stylesheet"/>
+		  <link href={wkCssFiraSansPath} rel="stylesheet"/>
+		  <link href={wkCssFiraRegularPath} rel="stylesheet"/>
+		  <Component/>
 		</>
 	);
 
