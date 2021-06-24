d2a0d7054f0260788514517c9df8e344912cf611
Damien Cassou <damien@cassou.me>
2021-06-24 11:46:33 +0200

    Delete useless profitAndLossChart.stories

--- src/components/profitAndLossChart/profitAndLossChart.stories.jsx
+++
@@ -1,9 +0,0 @@
-import ProfitAndLossChart from "./profitAndLossChart";
-import React from "react";
-
-export default {
-	component: ProfitAndLossChart,
-	title: "Components/ProfitAndLossChart"
-};
-
-export const shows = () => <ProfitAndLossChart/>;
