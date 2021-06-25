7960f750e530861139f551bd7f2958f9fbf0842e
Nicolas Petton <nicolas@petton.fr>
2021-06-25 14:31:21 +0200

    Fix dates in charts

--- src/widgets/profitAndLoss/profitAndLoss.jsx
+++ src/widgets/profitAndLoss/profitAndLoss.jsx
@@ -8,16 +8,16 @@ import registerComponent from "helpers/registerComponent";
 import ShowMoreButton from "components/showMoreButton/showMoreButton";
 
 const dates = [
-	new Date(2020, 0, 1),
-	new Date(2020, 1, 1),
-	new Date(2020, 2, 1),
-	new Date(2020, 3, 1),
-	new Date(2020, 4, 1),
-	new Date(2020, 5, 1),
-	new Date(2020, 6, 1),
-	new Date(2020, 7, 1),
-	new Date(2020, 8, 1),
-	new Date(2020, 9, 1)
+	new Date(2009, 0, 1),
+	new Date(2009, 1, 1),
+	new Date(2009, 2, 1),
+	new Date(2009, 3, 1),
+	new Date(2009, 4, 1),
+	new Date(2009, 5, 1),
+	new Date(2009, 6, 1),
+	new Date(2009, 7, 1),
+	new Date(2009, 8, 1),
+	new Date(2009, 9, 1)
 ];
 
 const data = {
