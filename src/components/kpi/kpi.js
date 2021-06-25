55622c51318ef97e2482c18be0c939f08d848ee0
Nicolas Petton <nicolas@petton.fr>
2021-06-25 13:27:41 +0200

    KPI draft

---
+++ src/components/kpi/kpi.js
@@ -0,0 +1,7 @@
+import data from "./kpi-data.json";
+import PropTypes from "prop-types";
+import React from "react";
+
+export default function KPI() {
+
+}
---
+++ src/components/kpi/kpi.stories.js
@@ -0,0 +1,9 @@
+import KPI from "./kpi";
+import React from "react";
+
+export default {
+	component: KPI,
+	title: "Components/KPI"
+};
+
+export const shows = () => <KPI/>;
